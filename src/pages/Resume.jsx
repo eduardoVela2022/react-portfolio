aa37e3d8dfde801d9ae0f8e8c4d14b6ca1b83110
eduardoVela <eduardovela2022@icloud.com>
2024-08-12 15:06:40 -0600

    Added style to the resume page, and added an anchor element to download my CV.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,14 +1,29 @@
 import PageTitle from "../components/PageTitle";
 import ResumeSkillList from "../components/ResumeSkillList";
+import CV_PATH from "../assets/CV.pdf";
 
 // The resume page
 function Resume() {
   const frontEndSkills = ["React", "Redux", "JavaScript", "CSS", "HTML"];
-  const backEndSkills = ["Node.js", "Express.js", "PostgreSQL", "MongoDB"];
+  const backEndSkills = [
+    "Node.js",
+    "Express.js",
+    "PostgreSQL",
+    "MongoDB",
+    "GraphQL",
+  ];
 
   return (
-    <div>
+    <div className="content-container">
       <PageTitle title="Resume" />
+
+      <a
+        className="resume-download-link"
+        href={CV_PATH}
+        download="ESVA-CV-EN-2.pdf"
+      >
+        Download CV
+      </a>
 
       <ResumeSkillList title="Front end skills" skills={frontEndSkills} />
 
