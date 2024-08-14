51fa5c092f7fe9601276e70ac67220bc5ba1eaea
eduardoVela <eduardovela2022@icloud.com>
2024-08-14 14:12:28 -0600

    Added style to the portfolio page and completed the project component.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,37 @@
 import PageTitle from "../components/PageTitle";
+import Project from "../components/Project";
+import TheJavaScriptBlogImage from "../assets/theJavaScriptBlog.png";
+import MyCurrenciesImage from "../assets/myCurrencies.png";
+import WebWeatherImage from "../assets/webWeather.png";
 
 // The portfolio page
 function Portfolio() {
   return (
-    <div>
+    <div className="content-container">
       <PageTitle title="Portfolio" />
+
+      <div className="portfolio-container scroll-container">
+        <Project
+          name="The JavaScript Blog project"
+          link="https://github.com/eduardoVela2022/JS-Blog"
+          imageSrc={TheJavaScriptBlogImage}
+          imageAlt="The JavaScript Blog project"
+        />
+
+        <Project
+          name="The My Currencies project"
+          link="https://github.com/eduardoVela2022/MyCurrencies"
+          imageSrc={MyCurrenciesImage}
+          imageAlt="The My Currencies project"
+        />
+
+        <Project
+          name="The Web Weather project"
+          link="https://github.com/eduardoVela2022/Web-Weather"
+          imageSrc={WebWeatherImage}
+          imageAlt="The Web Weather project"
+        />
+      </div>
     </div>
   );
 }
