0f49778671adc9deb04ebd003428ea824fc9d68e
eduardoVela <eduardovela2022@icloud.com>
2024-08-14 19:33:13 -0600

    Added form functionality and validation.

--- src/components/ResumeSkillListItem.jsx
+++ src/components/ResumeSkillListItem.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-// The resume skill list item
+// The resume skill list item component
 function ResumeSkillListItem({ skill }) {
   return <li>{skill}</li>;
 }
